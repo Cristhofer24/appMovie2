2e04b328e834276946553c3a66b48878f6f34767
cristhofer24 <you@example.com>
2024-12-13 10:51:58 -0500

    Firebase no toques nada de firebase por fa

--- lib/Screens/registro.dart
+++ lib/Screens/registro.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class RegistroScreen extends StatelessWidget {
   const RegistroScreen({super.key});
@@ -107,4 +108,22 @@ class RegistroScreen extends StatelessWidget {
       ),
     );
   }
+}
+
+Future<void> registro(emailAddress, password) async {
+  try {
+  // ignore: unused_local_variable
+  final credential = await FirebaseAuth.instance.createUserWithEmailAndPassword(
+    email: emailAddress,
+    password: password,
+  );
+} on FirebaseAuthException catch (e) {
+  if (e.code == 'weak-password') {
+    print('The password provided is too weak.');
+  } else if (e.code == 'email-already-in-use') {
+    print('The account already exists for that email.');
+  }
+} catch (e) {
+  print(e);
+}
 }
