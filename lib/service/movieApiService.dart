e36443be6b5ee953ef98eb37d7d293d14264d170
isantos <hancellsud@gmail.com>
2024-12-18 23:00:03 -0500

    cambios del API

--- lib/service/movieApiService.dart
+++ lib/service/movieApiService.dart
@@ -1,5 +1,5 @@
 import 'dart:convert';
-import 'package:http/http.dart' as http;
+import 'package:http_parser/http_parser.dart' as http;
 
 class TMDBService {
   // Tu token de autorización (API Key)
