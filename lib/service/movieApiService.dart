c936e4bbdf0f6272e11a827902905e7a4402631c
isantos <hancellsud@gmail.com>
2024-12-19 10:39:24 -0500

    envio de ajustes de compativilidad API- reproductor

--- lib/service/movieApiService.dart
+++ lib/service/movieApiService.dart
@@ -1,6 +1,6 @@
 import 'dart:convert';
-import 'package:http_parser/http_parser.dart' as http;
-import 'package:  http/http.dart' as http;
+//import 'package:http_parser/http_parser.dart' as http;
+import 'package:http/http.dart' as http;
 
 class TMDBService {
   // Tu token de autorización (API Key)
